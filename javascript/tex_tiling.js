4827fa42e493cbb0a8c444ec9d99bb9c8beabb44
cpetry <petry2christian@gmail.com>
2014-08-31 13:01:16 +0200

    added features

--- javascript/tex_tiling.js
+++ javascript/tex_tiling.js
@@ -1,6 +1,6 @@
 
 
-$('.color-box1').colpick({
+$('.tiles_color').colpick({
 	layout:'rgbhsbhex',
 	color: {h:200, s:0, b:100},
 	hue:200,
@@ -12,7 +12,17 @@ $('.color-box1').colpick({
 }).css('background-color', '#ffffff');
 
 
-$('.color-box2').colpick({
+$('.grout_color').colpick({
+	layout:'rgbhsbhex',
+	color: {h:200, s:0, b:0},
+	onSubmit:function(hsb,hex,rgb,el) {
+		$(el).css('background-color', '#'+hex);
+		$(el).colpickHide();
+		updateTiling();
+	}
+}).css('background-color', '#000000');
+
+$('.tiles_smooth_color').colpick({
 	layout:'rgbhsbhex',
 	color: {h:200, s:0, b:0},
 	onSubmit:function(hsb,hex,rgb,el) {
@@ -24,8 +34,9 @@ $('.color-box2').colpick({
 
 
 function updateTiling(){
-	var hex1 = rgb2hex($("#1").css("background-color"));
-	var hex2 = rgb2hex($("#2").css("background-color"));
+	var hex1 = rgb2hex($(".tiles_color").css("background-color"));
+	var hex2 = rgb2hex($(".grout_color").css("background-color"));
+	var hex3 = rgb2hex($(".tiles_smooth_color").css("background-color"));
 	
 	var x_tiling = parseInt($("#tiles_x").val());
 	var y_tiling = parseInt($("#tiles_y").val());
@@ -33,14 +44,17 @@ function updateTiling(){
 	var y_grout = parseInt($("#tiles_y_grout").val());
 	var x_smooth = parseInt($("#tiles_x_smooth").val());
 	var y_smooth = parseInt($("#tiles_y_smooth").val());
-	//document.write(x_tiling);
+
+	if (!$('#Smoothness').prop('checked')){
+		x_smooth = 0;
+		y_smooth = 0;
+	}
 	
-	// tile_col_hex, hori_count, vert_count, grout_col_hex, hori_gap, vert_gap
-	setTiling(hex1, x_tiling, y_tiling, hex2, x_grout, y_grout, x_smooth, y_smooth);
+	setTiling(hex1, x_tiling, y_tiling, hex2, x_grout, y_grout, hex3, x_smooth, y_smooth);
 }
 
 
-function setTiling(tile_col_hex, hori_count, vert_count, grout_col_hex, hori_gap, vert_gap, x_smooth, y_smooth)
+function setTiling(tile_col_hex, hori_count, vert_count, grout_col_hex, hori_gap, vert_gap, tiles_smooth_col, x_smooth, y_smooth)
 {
 	var c = document.getElementById("texture_preview");
 	var ctx = c.getContext("2d");
@@ -56,7 +70,7 @@ function setTiling(tile_col_hex, hori_count, vert_count, grout_col_hex, hori_gap
 	var tile_part_x = Math.max((max_w / hori_count) - hori_gap - (x_smooth * 2), 0);
 	var tile_part_y = Math.max((max_h / vert_count) - vert_gap - (y_smooth * 2), 0);	
 	
-	function gradient(dir, tile_d, gap_d, smooth_d, grout_col_hex, tile_col_hex) {
+	function gradient(dir, tile_d, gap_d, smooth_d, tile_col_hex, grout_col_hex, tiles_smooth_col) {
 		var grad = ctx.createLinearGradient(dir[0], dir[1], dir[2], dir[3]);
 		
 		var max_d = tile_d + gap_d + smooth_d*2;
@@ -75,9 +89,9 @@ function setTiling(tile_col_hex, hori_count, vert_count, grout_col_hex, hori_gap
 		return grad;
 	}
 	
-	function drawRectangle(tile_part_x, tile_part_y, hori_count, vert_count, hori_gap, vert_gap, x_smooth, y_smooth, grout_col_hex, tile_col_hex, ctx, x, y, w, h){
+	function drawRectangle(tile_part_x, tile_part_y, hori_count, vert_count, hori_gap, vert_gap, x_smooth, y_smooth, tile_col_hex, grout_col_hex, tiles_smooth_col, ctx, x, y, w, h){
 		
-		ctx.fillStyle = gradient([0, y, 0, h], tile_part_y, vert_gap, y_smooth, grout_col_hex, tile_col_hex);
+		ctx.fillStyle = gradient([0, y, 0, h], tile_part_y, vert_gap, y_smooth, tile_col_hex, grout_col_hex, tiles_smooth_col);
 		ctx.fillRect(x, y, w, h);
 
 		ctx.save();
@@ -128,8 +142,9 @@ function setTiling(tile_col_hex, hori_count, vert_count, grout_col_hex, hori_gap
 	for (var y=0; y < vert_count; y++){
 		for (var x=0; x < hori_count; x++){
 			drawRectangle(tile_part_x, tile_part_y, hori_count, vert_count, hori_gap, vert_gap,
-			x_smooth, y_smooth, grout_col_hex, tile_col_hex, 
-			ctx, x*max_w/hori_count, y*max_h/vert_count, (x+1)*max_w/hori_count, (y+1)*max_h/vert_count);
+			x_smooth, y_smooth, tile_col_hex, grout_col_hex, tiles_smooth_col,
+			ctx, Math.floor(x*max_w/hori_count), Math.floor(y*max_h/vert_count), 
+			Math.ceil((x+1)*max_w/hori_count), Math.ceil((y+1)*max_h/vert_count));
 		}
 	}
 	
