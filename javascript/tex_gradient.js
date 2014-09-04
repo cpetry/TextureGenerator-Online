6d5ffb0ba8b9e817daeedc1312d7b23f641e1ca6
Christian Petry <petry2christian@gmail.com>
2014-09-04 18:19:31 +0200

    added working gradient stuff

--- javascript/tex_gradient.js
+++ javascript/tex_gradient.js
@@ -1,4 +1,4 @@
-$('.test_color').colpick({
+$('.slider').colpick({
 	layout:'rgbhsbhex',
 	color: {h:35, s:15, b:90},
 	hue:200,
@@ -12,27 +12,113 @@ $('.test_color').colpick({
 }).css('background-color', '#e6d7c3');
 
 
-function updateGradient(){
-	var colors = [];
-	colors.push([rgb2hex($(".test_color").css("background-color")), 0]);
-	
-	setPreviewColors(colors);
-}
-
 $(".slider_area" ).click(function(evt) {
-	var x = evt.pageX - $(this).offset().left;
-	alert( "Position: " + x );
+	var x = Math.max(evt.pageX - $(this).offset().left, 0);
+	var new_slider = $('.slider')[0].cloneNode(true);
+	$(new_slider).css('left', Math.max(x - 5, 0));
+	
+	$(new_slider).draggable({ 
+		//axis: "x", 
+		grid: [ 1, 50 ],
+		//containment: ".slider_area", 
+		//scroll: false,
+		drag: function() {
+			updateGradient();
+		},
+	});
+	
+	$(new_slider).colpick({
+		layout:'rgbhsbhex',
+		color: {h:0, s:100, b:0},
+		hue:200,
+		onChange:function(hsb,hex,rgb,el) {
+			$(el).css('background-color', '#'+hex);
+			updateGradient();
+		},
+		onSubmit:function(hsb,hex,rgb,el) {
+			$(el).colpickHide();
+		}
+	}).css('background-color', '#ffffff');
+	
+	$('.slider_area').append(new_slider);
+	
+	updateGradient();
+	//alert( "Position: " + x );
 });
 
 
-function setPreviewColors(colors){
+$( ".slider" ).draggable({ 
+	axis: "x", 
+	grid: [ 1, 40 ],
+	//cursorAt: { left: -5 },
+	drag: function() {
+        updateGradient();
+    },
+});
+	
+$(".slider_area").droppable({
+	out: function (event, ui) {
+        $(ui.draggable).remove();
+    }
+});
+
+
+function updateGradient(){
+	var colors = [];
+	
+	$(".slider").each(function( index ) {
+		var pos = $(this).css("left");
+		pos = pos.substring(0, pos.length - 2);
+		var percentage = Math.min(parseFloat(pos) + 5, 256) / 2.56;
+	
+		colors.push([rgb2hex($(this).css("background-color")), percentage]);
+	});
+	
+	
+	gradient_type = $("select#gradient_type option:selected").val();
+	//document.write(gradient_type);
+	setGradientColors(colors, gradient_type);
+}
+
+function percentage_compare (a,b) {
+  // I'm assuming all values are numbers
+  return a[1] - b[1];
+}
+
+function setGradientColors(colors, gradient_type){
 	//document.write(colors[0][1]);
 	
 	// only the one the browser supports, will be applied!
-	$(".gradient_preview").css('background', colors[0][0]); // Old browsers 
-	$(".gradient_preview").css('background', '-webkit-linear-gradient(top, ' + colors[0][0] + ' ' + colors[0][1] + '%, #7db9e8 100%)'); // Chrome10+,Safari5.1+
-	$(".gradient_preview").css('background', '-webkit-gradient(linear, left top, right top, color-stop(' + colors[0][1] + '%,' + colors[0][0] + '),color-stop(100%, #7db9e8)'); // Chrome10+,Safari5.1+
-	$(".gradient_preview").css('background', 'linear-gradient(to right, ' + colors[0][0] + ' ' + colors[0][1] + '%, #7db9e8 100%)'); // W3C
+	
+	//$(".gradient_preview").css('background', colors[0][0]); // Old browsers 
+	//$(".gradient_preview").css('background', '-webkit-linear-gradient(top, ' + colors[0][0] + ' ' + colors[0][1] + '%, #7db9e8 100%)'); // Chrome10+,Safari5.1+
+	//$(".gradient_preview").css('background', '-webkit-gradient(linear, left top, right top, color-stop(' + colors[0][1] + '%,' + colors[0][0] + '),color-stop(100%, #7db9e8)'); // Chrome10+,Safari5.1+
+	//$(".gradient_preview").css('background', 'linear-gradient(to right, ' + colors[0][0] + ' ' + colors[0][1] + '%, #7db9e8 100%)'); // W3C
+	colors.sort(percentage_compare);
+	
+	
+	var c = document.getElementById("texture_preview");
+	var ctx = c.getContext("2d");
+	
+	var grad;
+	if (gradient_type == 'linear')
+		grad = ctx.createLinearGradient(0,0,512,0);
+		
+	else if (gradient_type == 'radial')
+		grad = ctx.createRadialGradient(256, 256, 0, 256, 256, 256); //x1,y1,r1 ,x1,y1,r1
+	
+	var gradient_text = 'linear-gradient(to right';
+	
+	colors.forEach(function(col) {
+		gradient_text += ', ' + col[0] + ' ' + Math.max(col[1],0) + '%';
+		grad.addColorStop(Math.max(col[1],0) / 100, col[0]);
+	});
+	
+	$(".gradient_preview").css('background', gradient_text); // W3C
+	
+	
+ctx.fillStyle = grad;
+	ctx.fillRect(0, 0, 512, 512);
 	
 	/*
 	background: #1e5799; // Old browsers 
@@ -47,3 +133,4 @@ function setPreviewColors(colors){
 	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=0 ); // IE6-9 
 	*/
 }
+
